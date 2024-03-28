592f19c20aa07c7d4f7efe440196e2adb363fa32
danielbosa <danielbosa95@gmail.com>
2024-03-28 18:49:29 +0100

    Slider: on click bonus functionality

--- js/script.js
+++ js/script.js
@@ -24,6 +24,9 @@ createApp({
             } else {
                 this.activeIndexSlide--;
             }
+        },
+        showUp(index){
+            this.activeIndexSlide = index;
         }
     },
     mounted(){
@@ -32,12 +35,7 @@ createApp({
 }).mount('#app');
 
 /*
-Milestone2:
-Al click dell'utente sulle frecce incrementare/ decrementare l'indice attivo facendo in modo che l'immagine visualizzata in grande cambi e la classe active si sposti sulla thumbnail corretta
-Milestone 3:
-Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso l'alto, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso il basso.
 Bonus:
-1- al click su una thumb, visualizzare in grande l'immagine corrispondente
 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
 */
