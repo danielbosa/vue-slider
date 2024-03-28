8559d10fed9e4c370dff934742cc3c58b96f6d64
danielbosa <danielbosa95@gmail.com>
2024-03-28 18:03:02 +0100

    Slide works on click

--- js/script.js
+++ js/script.js
@@ -10,9 +10,34 @@ createApp({
         }
     },
     methods: {
+        nextSlide(){
+            if(this.activeIndexSlide === (slides.length -1)){
+                this.activeIndexSlide = 0;
+            } else {
+                this.activeIndexSlide++;
+            }
 
+        },
+        prevSlide(){
+            if(this.activeIndexSlide === 0){
+                this.activeIndexSlide = (slides.length -1);
+            } else {
+                this.activeIndexSlide--;
+            }
+        }
     },
     mounted(){
 
     }
 }).mount('#app');
+
+/*
+Milestone2:
+Al click dell'utente sulle frecce incrementare/ decrementare l'indice attivo facendo in modo che l'immagine visualizzata in grande cambi e la classe active si sposti sulla thumbnail corretta
+Milestone 3:
+Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso l'alto, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso il basso.
+Bonus:
+1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+*/
